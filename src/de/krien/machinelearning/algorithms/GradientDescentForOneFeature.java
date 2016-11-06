cec4140c9fb1a4b5d00c6ee24e33580afcfc728b
Alex Krien <alexkrien@googlemail.com>
2016-11-06 20:52:20 +0100

    com1

--- src/de/krien/machinelearning/algorithms/GradientDescentForOneFeature.java
+++ src/de/krien/machinelearning/algorithms/GradientDescentForOneFeature.java
@@ -4,34 +4,33 @@ import de.krien.machinelearning.test.data.HouseData;
 import de.krien.machinelearning.test.object.House1A;
 
 import java.util.List;
+import java.util.Random;
 
 public class GradientDescentForOneFeature {
 
-    private final static int MAX_ITERATIONS = 5;
-    private final float LEARNING_RATE = 0.1f;      // alpha
-    private final float ACCURACY = 0.0001f;          // epsilon
+    private final static int MAX_ITERATIONS = 49999;
+    private final float LEARNING_RATE = 0.00000001f;        // alpha
+    private final float ACCURACY = 0.001f;                  // epsilon
 
-    private float intercept = 1f;                   // theta0
-    private float slope = 1f;                     // theta1
+    private float intercept;                           // theta0
+    private float slope;                               // theta1
     private float interceptOld;
     private float slopeOld;
 
     public static void main(String[] args) {
         GradientDescentForOneFeature gD = new GradientDescentForOneFeature();
-        gD.calculate(HouseData.ONE_FEATURE_HOUSE_DATA2);
+        gD.calculate(HouseData.ONE_FEATURE_HOUSE_DATA);
         System.out.println("Intercept: " +  gD.getIntercept());
         System.out.println("Slope: " +  gD.getSlope());
     }
 
     public void calculate(List<House1A> data) {
+        intercept = new Random().nextFloat() * 100;
+        slope = new Random().nextFloat() * 100;
+
         int iteration = 0;
         while(!isConvergent() && iteration++ < MAX_ITERATIONS) {
             iterate(data);
-            System.out.println("Converged:" + isConvergent());
-            System.out.println("Interations: " + iteration);
-            System.out.println("intercept: " + intercept);
-            System.out.println("slope: " + slope);
-            System.out.println("#################");
         }
     }
 
@@ -53,19 +52,17 @@ public class GradientDescentForOneFeature {
             interceptGradient = interceptGradient + calculateInterceptGradient(intercept, slope, house, data.size());
             slopeGradient = slopeGradient + calculateSlopeGradient(intercept, slope, house, data.size());
         }
-        System.out.println("interceptGradient: " + interceptGradient);
-        System.out.println("slopeGradient: " + slopeGradient);
         interceptOld = intercept;
         slopeOld = slope;
-        intercept = intercept - (LEARNING_RATE * interceptGradient);
-        slope = slope - (LEARNING_RATE * slopeGradient);
+        intercept = intercept - (LEARNING_RATE * (1/(float)data.size()) * interceptGradient);
+        slope = slope - (LEARNING_RATE * (1/(float)data.size()) * slopeGradient);
     }
 
     private float calculateInterceptGradient(float intercept, float slope, House1A house, int size) {
         float houseSize = house.getSizeInSquareMeters();
         float calculatedValue = intercept + (slope * houseSize);
         float realValue = house.getPriceInEuro();
-        float gradient = -(2/(float)size) * (calculatedValue - realValue);
+        float gradient = (calculatedValue - realValue);
         return gradient;
     }
 
@@ -73,7 +70,7 @@ public class GradientDescentForOneFeature {
         float houseSize = house.getSizeInSquareMeters();
         float calculatedValue = intercept + (slope * houseSize);
         float realValue = house.getPriceInEuro();
-        float gradient = -(2/(float)size) * houseSize * (calculatedValue - realValue);
+        float gradient = houseSize * (calculatedValue - realValue);
         return gradient;
     }
 
